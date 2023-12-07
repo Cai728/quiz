3253a16eb95ecb4ca635bdb07c078855ce35ece1
Cai <144351364+Cai728@users.noreply.github.com>
2023-12-07 19:49:49 +0000

    working quiz

--- index.js
+++ index.js
@@ -2,51 +2,76 @@ const questions = [
     {
         question: "What is the capital of France?",
         answer: "Paris",
-        possibleAnswers: ["Paris", "London", "Berlin", "Marid"]
+        possibleAnswers: ["Paris", "London", "Berlin", "Madrid"]
     },
     {
         question: "What is the capital of Spain?",
-        answer: "Madris",
-        possibleAnswers: ["Paris", "London", "Berlin", "Marid"]
+        answer: "Madrid",
+        possibleAnswers: ["Paris", "London", "Berlin", "Madrid"]
     },
     {
         question: "What is the capital of Germany?",
         answer: "Berlin",
-        possibleAnswers: ["Paris", "London", "Berlin", "Marid"]
+        possibleAnswers: ["Paris", "London", "Berlin", "Madrid"]
     },
     {
         question: "What is the capital of the UK?",
         answer: "London",
-        possibleAnswers: ["Paris", "London", "Berlin", "Marid"]
+        possibleAnswers: ["Paris", "London", "Berlin", "Madrid"]
     }
 ];
 
 let score = 0;
+let currentQuestionIndex = 0;
 
-function updateQuestion(question) {
-    document.getElementById("question").innerText = question.question
+function updateQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    document.getElementById("question").innerText = currentQuestion.question;
 }
 
-function renderOptions(question) {
-    document.getElementById("answer-radios").innerText = question.possibleAnswers
+function renderOptions() {
+    const currentQuestion = questions[currentQuestionIndex];
+    const radioContainers = document.getElementById("answer-radios");
+    radioContainers.innerHTML = '';
+
+    currentQuestion.possibleAnswers.forEach((answer, index) => {
+        const radioInput = document.createElement('input');
+        radioInput.type = 'radio';
+        radioInput.name = 'answer';
+        radioInput.value = answer;
+        radioInput.id = `answer${index + 1}`;
+
+        const label = document.createElement('label');
+        label.setAttribute('for', `answer${index + 1}`);
+        label.innerText = answer;
+
+        radioContainers.appendChild(radioInput);
+        radioContainers.appendChild(label);
+        radioContainers.appendChild(document.createElement('br'));
+    });
 }
 
 function moveToNextQuestion() {
-    const currentQuestionIndex = questions.indexOf(currentQuestion);
-    const nextQuestion = questions[currentQuestionIndex + 1]
-    updateQuestion(nextQuestion)
-    renderOptions(nextQuestion)
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        updateQuestion();
+        renderOptions();
+    } else {
+        alert(`Quiz completed! Your score: ${score}/${questions.length}`);
+    }
 }
 
 function checkAnswer() {
-    const usersGuess = document.getElementById("next-btn").value
-    if (usersGuess === currentQuestion.answer) {
-        score++
-        moveToNextQuestion()
+    const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    if (selectedAnswer) {
+        if (selectedAnswer.value === questions[currentQuestionIndex].answer) {
+            score++;
+        }
+        moveToNextQuestion();
+    } else {
+        alert('Please select an answer before moving to the next question.');
     }
-    
-    }
+}
 
-let currentQuestion = questions[0]
-updateQuestion(currentQuestion)
-renderOptions(currentQuestion)
+updateQuestion();
+renderOptions();
