dd86271b93a449757b8218fb42319a6d34641d3b
Cai <144351364+Cai728@users.noreply.github.com>
2023-12-08 15:59:20 +0000

    Version with contact form

--- index.js
+++ index.js
@@ -1,28 +1,91 @@
+function startIntro () {
+    document.getElementById("intro-wrapper").classList.remove("invisible")
+    document.getElementById("form-wrapper").classList.add("invisible")
+    
+    }
+
+const usersArr = [
+    {
+      firstName: "Cai",
+      secondName: "Panesar",
+      userId: 1234,
+    },
+  ];
+
+  const validateForm = () => {
+    let firstName = document.forms["myform"]["fname"].value;
+    let secondName = document.forms["myform"]["sname"].value;
+    let userId = document.forms["myform"]["uid"].value;
+    if (firstName == "") {
+      alert("name must be filled out");
+      return false;
+    } else if (secondName === "") {
+      alert("surname must be filled out");
+      return false;
+    } else if (userId?.length !== 4) {
+      alert("user id must be 4 characters");
+      return false;
+    } else if (isNaN(userId)) {
+      alert("user id must be a number");
+      return false;
+    }
+
+    const isValidUser = usersArr.find(
+      (user) => user.userId === Number(userId)
+    );
+    if (
+      isValidUser &&
+      firstName.toLowerCase() === isValidUser.firstName.toLowerCase() &&
+      secondName.toLowerCase() === isValidUser.secondName.toLowerCase()
+    ) {
+      alert("You are now logged in");
+      startIntro()
+      return false;
+    } else {
+      console.log("you can't log in");
+      return false;
+    }
+  };
+
+function startQuiz() {
+    document.getElementById("question-wrapper").classList.remove("invisible")
+    document.getElementById("intro-wrapper").classList.add("invisible")
+}
+
+function testClick () { //used an function to call the two other functions for the onclick button
+    if (validateForm())
+    {
+        startIntro()
+    } else {
+
+    }
+}
+
 const questions = [
     {
-        question: "What is the capital of France?",
-        answer: "Paris",
-        possibleAnswers: ["Paris", "London", "Berlin", "Madrid"]
+        question: "Which is the smallest country in the world?",
+        answer: "Vatican City",
+        possibleAnswers: ["Micronesia", "Iceland", "Vatican City", "Microstan"]
     },
     {
-        question: "What is the capital of Spain?",
-        answer: "Madrid",
-        possibleAnswers: ["Paris", "London", "Berlin", "Madrid"]
+        question: "Which language has the most native speakers?",
+        answer: "Mandarin",
+        possibleAnswers: ["Mandarin", "Spanish", "Arabic", "English"]
     },
     {
-        question: "What is the capital of Germany?",
-        answer: "Berlin",
-        possibleAnswers: ["Paris", "London", "Berlin", "Madrid"]
+        question: "Which artist has the most streams on Spotify?",
+        answer: "Drake",
+        possibleAnswers: ["Justin Bieber", "Drake", "Taylor Swift", "Rihanna"]
     },
     {
-        question: "What is the capital of the UK?",
-        answer: "London",
-        possibleAnswers: ["Paris", "London", "Berlin", "Madrid"]
+        question: "What year was the United Nations established?",
+        answer: "1945",
+        possibleAnswers: ["1946", "1945", "1919", "1901"]
     }
 ];
 
 let score = 0;
-let currentQuestionIndex = 0;
+let currentQuestionIndex = 0; //Made this an global variable
 
 function updateQuestion() {
     const currentQuestion = questions[currentQuestionIndex];
@@ -57,8 +120,15 @@ function moveToNextQuestion() {
         updateQuestion();
         renderOptions();
     } else {
-        alert(`Quiz completed! Your score: ${score}/${questions.length}`);
+        document.getElementById("scoreP").innerText = `Quiz completed! Your score: ${score}/${questions.length}`
+        scorePage()
     }
+}
+
+function scorePage() {
+    document.getElementById("question-wrapper").classList.add("invisible")
+    document.getElementById("end-wrapper").classList.remove("invisible")
+    moveToNextQuestion()
 }
 
 function checkAnswer() {
@@ -75,3 +145,4 @@ function checkAnswer() {
 
 updateQuestion();
 renderOptions();
+
